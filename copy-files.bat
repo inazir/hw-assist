4bfdabe7cb6fd66c37ff2f797dc3c3c2d0609e8b
Iqbal Nazir <iqbalnazirsumon@gmail.com>
2022-04-10 15:16:24 +0200

    Modify copy files

--- copy-files.bat
+++ copy-files.bat
@@ -1,3 +1,9 @@
-md "C:\hw-assist\all_homeworks"
-cd /d "C:\hw-assist\homework"
-for /r %%d in (*) do copy "%%d" "C:\hw-assist\all_homeworks\"
+set main_homework="C:\hw-assist\homework"
+set new_homework="C:\hw-assist\all_homeworks\"
+if not exist %new_homework% (
+	echo folder does not exists. creating...
+	md %new_homework%
+    )
+
+cd /d %main_homework%
+for /r %%d in (*) do copy "%%d" %new_homework%
